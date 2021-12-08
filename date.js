cb396e85d36e35ffa254790da541a8ee83f60bf7
Sadid369 <sadid.jones@gmail.com>
2021-12-08 22:42:27 +0600

    first commit

---
+++ date.js
@@ -0,0 +1,8 @@
+var date = new Date();
+
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleString());
+console.log(date.getFullYear());
+console.log(date.getMonth() + 1);
+console.log(date.getDate());
